7b2c733db26df4867e84b6e663643c69576c2cac
Wolfram Hofmeister <wolfram.hofmeister@bitmovin.net>
2015-09-16 12:13:31 +0200

    fixed sign-up-button visibility

--- app/src/main/java/com/bitmovin/bitcodin/ErrorPopup.java
+++ app/src/main/java/com/bitmovin/bitcodin/ErrorPopup.java
@@ -6,23 +6,32 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.text.Html;
+import android.view.View;
 
 public class ErrorPopup {
   private AlertDialog mAlertDialog;
-  private Context context;
 
-  public ErrorPopup(Context context) {
+  public ErrorPopup(final Context context) {
     this.mAlertDialog = new AlertDialog.Builder(context).create();
     this.mAlertDialog.setCancelable(false);
     this.mAlertDialog.setMessage("alert");
-    this.context = context;
     this.mAlertDialog.setButton(DialogInterface.BUTTON_NEUTRAL, "OK",
         new DialogInterface.OnClickListener() {
-      @Override
-      public void onClick(DialogInterface dialog, int which) {
-        dialog.dismiss();
-      }
-    });
+          @Override
+          public void onClick(DialogInterface dialog, int which) {
+            dialog.dismiss();
+          }
+        });
+    this.mAlertDialog.setButton(DialogInterface.BUTTON_POSITIVE, "sign up",
+        new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialog, int which) {
+            Intent viewHomepageIntent = new Intent(Intent.ACTION_VIEW);
+            viewHomepageIntent.setData(Uri.parse("https://www.bitcodin.com/sign-up/"));
+            context.startActivity(viewHomepageIntent);
+            dialog.dismiss();
+          }
+        });
   }
 
   public void show(String message) {
@@ -31,18 +40,9 @@ public class ErrorPopup {
 
   public void show(String message, boolean showRegisterButton) {
     this.mAlertDialog.setMessage(Html.fromHtml(message));
-    if (showRegisterButton) {
-      this.mAlertDialog.setButton(DialogInterface.BUTTON_POSITIVE, "sign up",
-          new DialogInterface.OnClickListener() {
-        @Override
-        public void onClick(DialogInterface dialog, int which) {
-          Intent viewHomepageIntent = new Intent(Intent.ACTION_VIEW);
-          viewHomepageIntent.setData(Uri.parse("https://www.bitcodin.com/sign-up/"));
-          context.startActivity(viewHomepageIntent);
-          dialog.dismiss();
-        }
-      });
-    }
     this.mAlertDialog.show();
+    this.mAlertDialog.getButton(DialogInterface.BUTTON_POSITIVE).setVisibility(
+        showRegisterButton ? View.VISIBLE : View.GONE
+    );
   }
 }
